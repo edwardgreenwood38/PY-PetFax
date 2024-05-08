0a9c90d7c40aa80f3887eacfe0dff418c39db540
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-05-07 21:29:19 -0400

    view added

---
+++ petfax/pet.py
@@ -0,0 +1,9 @@
+from flask import ( Blueprint, render_template )
+
+bp = Blueprint('pet', __name__, url_prefix="/pets")
+
+
+# routes
+@bp.route('/')
+def index(): 
+    return render_template('index.html')
