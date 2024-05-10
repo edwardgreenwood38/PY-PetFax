84284abb2522dcb3d743a0c0cd24f15d6a77dfd7
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-05-09 21:38:06 -0400

    show page and basic nav

--- petfax/pet.py
+++ petfax/pet.py
@@ -11,3 +11,9 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 @bp.route('/')
 def index(): 
     return render_template('index.html', pets=pets)
+
+
+@bp.route('/<pet_id>')
+def show(pet_id): 
+    pet = [pet for pet in pets if pet['pet_id'] == int(pet_id)][0]
+    return render_template('show.html', pet=pet)
